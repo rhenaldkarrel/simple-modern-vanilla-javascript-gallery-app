3c4dd6b4186f54dd80f847e00f2ff0c3f2a7c340
Rhenald <renaldkarel@gmail.com>
2023-05-30 20:28:10 +0700

    feat: search functionality on gallery page

--- app/pages/gallery.js
+++ app/pages/gallery.js
@@ -1,5 +1,27 @@
 import { loadNav } from '../ui/navbar.js';
+import { loadPhotos } from '../ui/photos.js';
+import { DOM } from '../utils/index.js';
+import { gallery } from '../constants/images-data.js';
 
 document.addEventListener('DOMContentLoaded', function () {
+	const photosContainerClass = '.photos .row';
+
 	loadNav();
+	loadPhotos(photosContainerClass, gallery);
+
+	const searchForm = DOM.find('.search-form');
+
+	searchForm.addEventListener('submit', function (e) {
+		e.preventDefault();
+
+		const query = DOM.find('#search-input').value;
+
+		if (query === '') {
+			return loadPhotos(photosContainerClass, gallery);
+		}
+
+		const result = gallery.filter((p) => p.category.includes(query));
+
+		loadPhotos(photosContainerClass, result);
+	});
 });
